dc1e4421f66ed44a49a3aad01a991cabec51877f
Thomas Vathonne <2249740+zlezero@users.noreply.github.com>
2020-10-11 15:47:24 +0100

    feat(webpack): config webpack + exemple avec page login

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,8 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('app', './assets/entrypoint/app.js')
+    .addEntry('login', './assets/entrypoint/login.js')
     //.addEntry('page1', './assets/page1.js')
     //.addEntry('page2', './assets/page2.js')
 
@@ -54,7 +55,7 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
