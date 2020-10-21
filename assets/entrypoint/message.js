9e50d3cd5154acd6cc359d3a61527664cfb5b173
Thomas Vathonne <2249740+zlezero@users.noreply.github.com>
2020-10-21 07:52:00 +0100

    feat(emojis) : ajout des emojis

--- assets/entrypoint/message.js
+++ assets/entrypoint/message.js
@@ -1,3 +1,10 @@
 import '../js/message';
+import '../styles/message.css';
+import '../styles/message.scss';
+
+import '../plugins/emoji-picker-twemoji/js/twemoji.min.js';
+import '../plugins/emoji-picker-twemoji/js/DisMojiPicker';
+import '../plugins/emoji-picker-twemoji/css/emojis.css';
+
 const WS = require('../../vendor/gos/web-socket-bundle/public/js/websocket.min.js');
 global.WS = WS;
--- assets/js/message.js
+++ assets/js/message.js
@@ -1,13 +1,42 @@
 $(function() {
+    
+
+    //Gestion des emojis
+
+    $("#emojis").disMojiPicker();
+    twemoji.parse(document.body);
+
+    $("#emojis").picker(
+        emoji => $('#message').val($('#message').val() + emoji)
+    );
+
+    $('#emojis').css('display', 'none');
+
+    $('#btnEmojis').on('click', (e) => {
+       $('#emojis').css('display') == "none" ? $('#emojis').css('display', '') : $('#emojis').css('display', 'none');
+    });
+
+    $(document).on('mouseup', function(e){
+
+        var container = $("#emojis");
+    
+        if(!container.is(e.target) && container.has(e.target).length === 0){
+            container.hide();
+        }
+
+    });
+
+    //Gestion des messages
 
     const socket = WS.connect("ws://localhost:1337");
     var session_glob;
     var current_channel_id = 1;
 
     socket.on("socket/connect", function (session) {
+
         console.log("Connexion réussie !");
         session_glob = session;
-        // The callback function in "subscribe" is called every time an event is published in that channel.
+
         session.subscribe("message/channel", function (uri, payload) {
             console.log("Message reçu : ", payload);
             addMessage(payload.pseudo, payload.message, payload.channel);
@@ -27,27 +56,43 @@ $(function() {
         };
 
         session_glob.publish("message/channel", {data: JSON.stringify(data)});
-        //addMessage(message.name, message.message);
+        $('#message').val('');
 
     });
 
-    $('#changeChannel').on("click", function() {
-        if (current_channel_id == 1) {
-            current_channel_id = 2;
-        } else {
-            current_channel_id = 1;
+    $('#message').on('keyup', (e) => {
+
+        if (e.defaultPrevented) {
+            return;
         }
-        console.log("New channel id : " + current_channel_id);
+    
+        var key = e.key || e.keyCode;
+
+        if (key === 'Enter' || key === 13) {
+            e.preventDefault();
+            $('#sendBtn').trigger('click');
+        }
+
     });
 
     function addMessage(name, message, channel) {
-        const messageHTML = "<div class='message'><strong>" + name + ":</strong> " + message + "</div>";
-        console.log(channel);
-        if (channel == 2) {
-            $('#chat2').html($('#chat2').html() + messageHTML);
-        } else {
-            $('#chat').html($('#chat').html() + messageHTML);
-        }
+        const messageHTML = "<div class='col-12'><div class='chat-bubble chat-bubble--left'>" + name + " : " + message + "</div></div>";
+        $('#chat-messages').html($('#chat-messages').html() + messageHTML);
+    }
+
+    //Autres
+    var dropdown = document.getElementsByClassName("dropdown-btn");
+    var i;
+    for (i = 0; i < dropdown.length; i++) {
+        dropdown[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var dropdownContent = this.nextElementSibling;
+            if (dropdownContent.style.display === "block") {
+                dropdownContent.style.display = "none";
+            } else {
+                dropdownContent.style.display = "block";
+            }
+        });
     }
 
 });
